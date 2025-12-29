1ce076304903b5d0bb7cd1f45962989d0fb7a12b
DheerajSai24 <dheerajsai628@gmail.com>
2025-12-30 01:50:03 +0530

    Update project: Personalization, Fixes, and AI Integration setup

--- client/src/components/home/Title.jsx
+++ client/src/components/home/Title.jsx
@@ -1,12 +1,10 @@
-import React from 'react'
-
-const Title = ({title , description}) => {
+const Title = ({ title, description }) => {
   return (
-    <div className='text-center mt-3 text-slate-700'>
-        <h2 className='text-3xl sm:text-4xl font-medium'>{title}</h2>
-        <p className='max-sm max-w-2xl mt-2 text-slate-500'>{description}</p>
+    <div className="text-center mt-6 text-slate-700">
+      <h2 className="text-3xl sm:text-4xl font-extrabold">{title}</h2>
+      <p className="max-sm max-w-2xl mt-4 text-slate-500">{description}</p>
     </div>
-  )
-}
+  );
+};
 
-export default Title
+export default Title;
--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,10 +1,9 @@
-import React from 'react'
-import Banner from '../components/home/Banner'
-import Hero from '../components/home/Hero'
-import Features from '../components/home/Features'
-import Testimonial from '../components/home/Testimonial'
-import CallToAction from '../components/home/CallToAction'
-import Footer from '../components/home/Footer'
+import React from "react";
+import Banner from "../components/Home/Banner";
+import Hero from "../components/Home/Hero";
+import Features from "../components/Home/features";
+import CallToAction from "../components/Home/CallToAction";
+import Footer from "../components/Home/Footer";
 
 const Home = () => {
   return (
@@ -12,11 +11,10 @@ const Home = () => {
       <Banner />
       <Hero />
       <Features />
-      <Testimonial />
       <CallToAction />
       <Footer />
     </div>
-  )
-}
+  );
+};
 
-export default Home
+export default Home;
